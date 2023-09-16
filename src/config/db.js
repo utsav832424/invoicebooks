f95524605b5f1002f021640304a519dac82aa641
utsav832424 <bhikadiyautsav67@gmail.com>
2023-09-16 23:53:58 +0530

    chnage url of db

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,14 @@
 import mysql from "mysql";
-
-const pool = mysql.createConnection({
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB,
-    connectionLimit: 10
-});
+const urlDB = `mysql://root:jTulh0gn00MXVg6ULIOZ@containers-us-west-97.railway.app:7752/railway`;
+const pool = mysql.createConnection(urlDB);
+// const pool = mysql.createConnection({
+//     port: process.env.DB_PORT,
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.MYSQL_DB,
+//     connectionLimit: 10
+// });
 
 pool.connect((err) => {
     if (err) throw err;
